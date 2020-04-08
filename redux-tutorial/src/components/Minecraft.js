9bee9876d7a5c5cb58c2c3e1ded581a8687f228f
rand0819 <robertwinstonanderson@gmail.com>
2020-04-07 18:56:24 -0600

    add Minecraft component

---
+++ redux-tutorial/src/components/Minecraft.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Minecraft = () => {
+    return (
+        <div className="wrapper">
+            <div className="wrapper__item">
+                <img src="/images/pickaxe.png" alt="" />
+                <button>Mine</button>
+            </div>
+            <div className="wrapper__item">
+                <img src="/images/diamond.png" alt="" />
+                <span>{ /* incrementing number */}</span>
+            </div>
+        </div>
+    )
+}
