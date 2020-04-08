b22b3ac2c31daa3ae0eb0cc7dbc76f5024538eca
rand0819 <robertwinstonanderson@gmail.com>
2020-04-07 19:26:42 -0600

    add initial components and images

--- redux-tutorial/src/components/Minecraft.js
+++ redux-tutorial/src/components/Minecraft.js
@@ -4,13 +4,15 @@ const Minecraft = () => {
     return (
         <div className="wrapper">
             <div className="wrapper__item">
-                <img src="/images/pickaxe.png" alt="" />
+                <img src="images/diamond.png" alt="" />
                 <button>Mine</button>
             </div>
             <div className="wrapper__item">
-                <img src="/images/diamond.png" alt="" />
+                <img src="../images/diamond.png" alt="" />
                 <span>{ /* incrementing number */}</span>
             </div>
         </div>
     )
 }
+
+export default Minecraft;
